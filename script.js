b22964d107797dacdea0e178ea26af1f0bf290ec
Nicola Tabai <nicola.tabai98@gmail.com>
2023-11-02 16:17:16 +0100

    add for cycle

--- script.js
+++ script.js
@@ -8,3 +8,15 @@
 //     }
 //   }).mount('#app')
 
+
+console.log('ciao2')
+for(let i = 0; i < 10; i++) {
+    axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) => {
+        console.log(res.data)
+        // console.log(res)
+        // console.log('ciao')
+    
+    })
+}
