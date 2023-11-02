505697672a9e6d63a23520517fbe09c2bc2d3453
Nicola Tabai <nicola.tabai98@gmail.com>
2023-11-02 17:12:42 +0100

    add cycle with vue

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-// const { createApp } = Vue
+const { createApp } = Vue
 
-//   createApp({
-//     data() {
-//       return {
-//         message: 'Hello Vue!'
-//       }
-//     }
-//   }).mount('#app')
+  createApp({
+    data() {
+      return {
+        mailList: [],
+        message: 'hello',
+        mail: ''
 
 
-console.log('ciao2')
-for(let i = 0; i < 10; i++) {
-    axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((res) => {
-        console.log(res.data)
-        // console.log(res)
-        // console.log('ciao')
+      }
+    },
+    methods: {
+      fetchMail() {
+        for(let i = 0; i < 10; i++) {
+          axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                const mail = res.data.response;
+                this.mail = mail;
+                console.log(this.mail);
+                setTimeout(() => {
+                  this.mailList.push(mail)
+                }, 3000);
+          })
+        }
+        console.log(this.mailList)
+      },
+    },
+    created() {
+      this.fetchMail()
+    },
+
+  }).mount('#app')
+
+
+// console.log('ciao2')
+// for(let i = 0; i < 10; i++) {
+//     axios
+//         .get('https://flynn.boolean.careers/exercises/api/random/mail')
+//         .then((res) => {
+//         console.log(res.data.response)
+//         // console.log(res)
+//         // console.log('ciao')
     
-    })
-}
+//     })
+// }
